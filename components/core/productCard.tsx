e1fe5639818f93a9a1cb09ff6b00149deeb09c94
Pritimoy Paul <pritimoy97@gmail.com>
2025-01-12 22:26:22 +0600

    product card updated

--- components/core/productCard.tsx
+++ components/core/productCard.tsx
@@ -6,10 +6,18 @@ interface ProductCardProps {
   name: string;
   details: string;
   image: string;
+  quantity: number;
+  price: number;
   url: string;
 }
 
-const ProductCard = ({ name, details, image }: ProductCardProps) => {
+const ProductCard = ({
+  name,
+  details,
+  image,
+  quantity,
+  price,
+}: ProductCardProps) => {
   return (
     <div className="group bg-[#ffffff] w-full h-[238px] flex flex-col justify-center items-center py-[44px] rounded-[12px] relative hover:bg-[#ffffff] border-[1px] border-[#007AFF21] hover:border-[1px] hover:border-[#007AFF99]">
       <div className="h-[98px] w-[98px] mb-[20px]">
@@ -25,10 +33,10 @@ const ProductCard = ({ name, details, image }: ProductCardProps) => {
       <p className="text-[14px] text-[#8A94A6]">{details}</p>
       <div className="absolute bottom-[0px] w-full bg-[#FBFBFB] flex items-center justify-between px-[20px] rounded-b-[12px]">
         <span className="text-[#8A94A6] text-[14px] py-[7px] cursor-pointer">
-          50 G
+          {quantity}
         </span>
         <span className="text-[#007AFF] text-[14px] py-[7px] cursor-pointer">
-          Tk 50.00
+          Tk {price}
         </span>
       </div>
     </div>
